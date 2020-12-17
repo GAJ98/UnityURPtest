007b1ba638241323ea6bf5c29479566a41be9b57
GAJ98 <gregoryalexisjohn@live.in>
2020-12-17 03:46:08 -0500

    Converted to Unity Standard Asset Ethan model

---
+++ GhostBusters URP test/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/CamController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 1;
+    [SerializeField] Transform Player;
+    [SerializeField] Transform Target;
+
+    private float mouseX, mouseY;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        CamControl();
+    }
+
+    void CamControl()
+    {
+        mouseX = mouseX + Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY = mouseY - Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(Target);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        }
+        else
+        {
+            Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+}
+
+
