a9db10f5c05178652ecd9ac9d0b5a41cc6c391fd
GAJ98 <gregoryalexisjohn@live.in>
2020-12-22 16:50:17 -0500

    Added toggle for torch light on player model

---
+++ GhostBusters URP test/Assets/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    [SerializeField] GameObject torch_light;
+
+    bool state = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonUp("Torch"))
+        {
+            if(state == true)
+            {
+                torch_light.SetActive(false);
+                state = false;
+            }
+            else
+            {
+                torch_light.SetActive(true);
+                state = true;
+            }
+        }
+    }
+}
